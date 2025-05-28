729dc58d15a9d1b72c37e2d986a8657be54824eb
Anuj-puri07 <anujpuri0923@gmail.com>
2025-05-28 16:29:44 +0545

    updated

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
 
 app.listen(2900,function (){
     console.log("Backend has started at port 2900")
+})
+
+app.get("/",(req,res)=>{
+    res.send("this is my home page")
+})
+
+app.get("/contact",(req,res)=>{
+    res.render("contact", {age : 23, name : "about"})
+})
+
+app.get("/land", (req,res)=>{
+    let name ="ANuj puri"
+    res.render("haha", {firstname : name})
+})
+app.get("/aboutt", (req,res)=>{
+    res.render("about")
 })
---
+++ database/db.js
@@ -0,0 +1,7 @@
+// here the database connection code is written
+
+//step 1 import sequelize
+const {Sequilize} = require("sequelize")
+// const Sequelize = require("sequelize").Sequelize
+
+const sequelize = new Sequilize(`localost`,``)
