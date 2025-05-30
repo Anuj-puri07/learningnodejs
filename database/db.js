ac72450af906700105bbf2ee91a75bcb4f28b13d
Anuj-puri07 <anujpuri0923@gmail.com>
2025-05-30 15:56:20 +0545

    day 4

--- database/db.js
+++ database/db.js
@@ -1,7 +1,28 @@
 // here the database connection code is written
 
 //step 1 import sequelize
-const {Sequilize} = require("sequelize")
+const {Sequelize} = require("sequelize")
 // const Sequelize = require("sequelize").Sequelize
 
-const sequelize = new Sequilize(`localost`,``)
+require("dotenv").config() //after this syntax we can access the data of env file here
+
+const sequelize = new Sequelize({
+    database : process.env.database_name,
+    username : process.env.database_username,
+    password: process.env.database_password,
+    port : process.env.database_port,
+    host : process.env.database_host,
+    dialect : "mysql"
+})
+
+//this above code is used to connect sequilize with the database
+sequelize.authenticate()
+.then(()=>{
+    console.log("Conneccted Successfully");
+})
+
+.catch((err) =>{
+    console.log("Error connecting database", err);
+})
+
+module.exports = sequelize
